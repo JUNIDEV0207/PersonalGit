33dc631ae54db10688738fd5a3b302f9087a4b53
Christian Nisperos <chrisnis24@gmail.com>
2021-12-21 23:18:35 -0800

    finalized file

--- Seleniumtest2.py
+++ Seleniumtest2.py
@@ -1,18 +1,22 @@
-# import time
-# from selenium import webdriver
-
-# driver = webdriver.Chrome("C:\\Users\\Administrator\\Downloads\\chromedriver_win32\\chromedriver.exe")  # Optional argument, if not specified will search path.
-# driver.get('http://www.google.com/');
-# time.sleep(5) # Let the user actually see something!
-# search_box = driver.find_element_by_name('q')
-# search_box.send_keys('ChromeDriver')
-# search_box.submit()
-# time.sleep(5) # Let the user actually see something!
-# driver.quit()
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+PATH = "C:\\Users\\Administrator\\Downloads\\chromedriver_win32\\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
 
-driver = webdriver.Chrome("C:\\Users\\Administrator\\Downloads\\chromedriver_win32\\chromedriver.exe")
-driver.get("http://www.google.com")
-print(driver.title)
-driver.close()
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors-spki-list')
+options.add_argument('--ignore-ssl-errors')
+driver.get("https://www.google.com")
+
+search = driver.find_element_by_name("q")
+
+search.send_keys("google")
+search.send_keys(Keys.RETURN)
+
+results = driver.find_elements_by_xpath("//div[@class='kno-rdesc']/span")
+for value in results:
+    print(value.text)
