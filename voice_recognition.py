c06a306118d4dd74e52a1b2bd3900ca0991e76dd
Christian Nisperos <chrisnis24@gmail.com>
2021-12-21 18:01:44 -0800

    added voice recognition

---
+++ voice_recognition.py
@@ -0,0 +1,77 @@
+import speech_recognition as sr
+import pyttsx3 
+
+r = sr.Recognizer()
+m = sr.Microphone()
+
+try:
+    print("A moment of silence, please...")
+    with m as source: r.adjust_for_ambient_noise(source)
+    print("Set minimum energy threshold to {}".format(r.energy_threshold))
+    while True:
+        print("Say something!")
+        with m as source: audio = r.listen(source)
+        print("Got it! Now to recognize it...")
+        try:
+            # recognize speech using Google Speech Recognition
+            value = r.recognize_google(audio)
+
+            # we need some special handling here to correctly print unicode characters to standard output
+            if str is bytes:  # this version of Python uses bytes for strings (Python 2)
+                print(u"You said {}".format(value).encode("utf-8"))
+            else:  # this version of Python uses unicode for strings (Python 3+)
+                print("You said {}".format(value))
+        except sr.UnknownValueError:
+            print("Oops! Didn't catch that")
+        except sr.RequestError as e:
+            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+except KeyboardInterrupt:
+    pass
+
+
+  
+# Initialize the recognizer 
+r = sr.Recognizer() 
+  
+# Function to convert text to
+# speech
+def SpeakText(command):
+      
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command) 
+    engine.runAndWait()
+      
+      
+# Loop infinitely for user to
+# speak
+  
+while(1):    
+      
+    # Exception handling to handle
+    # exceptions at the runtime
+    try:
+          
+        # use the microphone as source for input.
+        with sr.Microphone() as source2:
+              
+            # wait for a second to let the recognizer
+            # adjust the energy threshold based on
+            # the surrounding noise level 
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+              
+            #listens for the user's input 
+            audio2 = r.listen(source2)
+              
+            # Using ggogle to recognize audio
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+  
+            print("Did you say "+MyText)
+            SpeakText(MyText)
+              
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+          
+    except sr.UnknownValueError:
+        print("unknown error occured")
