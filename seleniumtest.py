62fba694b403fd93884832ebeddf1e539ae3f305
Christian Nisperos <chrisnis24@gmail.com>
2021-12-21 18:04:38 -0800

    added selenium test files

---
+++ seleniumtest.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+driver = webdriver.Chrome(executable_path="C:\\Users\\Administrator\\Downloads\\chromedriver_win32\\chromedriver.exe")
+driver.get("http://www.google.com")
+
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors-spki-list')
+options.add_argument('--ignore-ssl-errors')
+driver = webdriver.Chrome(chrome_options=options)
+
+element = driver.find_element_by_name("q")
+element.send_keys("catdog")
+element.submit()
+
+results = driver.find_elements_by_xpath("//div[@class='g']//div[@class='r']//a[not(@class)]");
+for result in results:
+    print(result.get_attribute("href"))
